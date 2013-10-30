560c9f3d306a707e4944d1167dd4a907ea92e90f
Ali El-Ashram <ali.elashram@gmail.com>
2013-10-30 14:10:29 -0700

    Update navbar.

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -2,4 +2,7 @@ class Goal < ActiveRecord::Base
   attr_accessible :name, :user_id
   belongs_to :user
   has_many :plays
+
+  validates :name, presence: true,
+               length: { maximum: 140 }
 end
--- app/models/play.rb
+++ app/models/play.rb
@@ -2,4 +2,7 @@ class Play < ActiveRecord::Base
   attr_accessible :play, :notes, :goal_id
   belongs_to :goal
   belongs_to :user
+
+  validates :play, presence: true,
+               length: { maximum: 35 }
 end
